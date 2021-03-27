6317167874d550ff500b8b86caa1980eb5bc2bb8
Norbert <38264435+NBanski@users.noreply.github.com>
2021-03-27 14:07:29 +0100

    Update and rename useradd.sh to user-deploy.sh

---
+++ linux/user-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if test "$#" -lt "1" || test "$1" = "--help" || test "$1" = "-h"; then
+  echo "user-deploy.sh [username] [ssh]"
+  echo "username, new user's name"
+  echo "ssh, if present, root ssh keys are copied"
+  exit 1
+fi
+
+adduser --gecos GECOS $1
+usermod -aG sudo $1
+
+if test "$2" = ssh; then
+  mkdir /home/$1/.ssh
+  cp /.ssh/authorized_keys /home/$1/.ssh/authorized_keys
+  echo "SSH keys copied."
+fi
