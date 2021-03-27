905102c1939956091984ec5304d3d09537be1fc6
Norbert <38264435+NBanski@users.noreply.github.com>
2021-03-27 13:13:08 +0100

    Update docker-engine.sh

--- linux/docker-engine.sh
+++ linux/docker-engine.sh
@@ -4,8 +4,12 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
 # Add Docker’s official GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Take a breather.
+sleep 5
 # Set up stable repository:
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Take it slowly...
+sleep 5
 # Install Docker Engine:
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 # Run test:
