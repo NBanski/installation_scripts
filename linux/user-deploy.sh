60e04d0a81f57a5349dd5bfe448f5ef8d7fd9ea8
Norbert <38264435+NBanski@users.noreply.github.com>
2021-08-26 15:26:33 +0200

    Update user-deploy.sh

--- linux/user-deploy.sh
+++ linux/user-deploy.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-if test "$#" -lt "1" || test "$1" = "--help" || test "$1" = "-h"; then
-  echo "user-deploy.sh [username] [ssh]"
-  echo "username, new user's name"
-  echo "ssh, if present, root ssh keys are copied"
-  exit 1
-fi
-
-adduser --gecos GECOS $1
-usermod -aG sudo $1
-
-if test "$2" = "ssh"; then
-  mkdir /home/$1/.ssh
-  cp $HOME/.ssh/authorized_keys /home/$1/.ssh/authorized_keys
-  chown --recursive $1:$1 /home/$1/.ssh
-  echo "SSH keys copied."
-fi
+while getopts ":u:sf:h" opt; do
+        case ${opt} in
+        u )
+                username=$OPTARG
+                adduser --gecos "" $USERNAME
+                usermod -aG sudo $USERNAME
+                ;;
+        s )
+                mkdir /home/$USERNAME/.ssh
+                cp $HOME/.ssh/authorized_keys /home/$USERNAME/.ssh/authorized_keys
+                chown --recursive $USERNAME:$USERNAME /home/$USERNAME/.ssh
+                echo "SSH keys copied!"
+                ;;
+        f )
+                PORT=$OPTARG
+                sudo ufw enable 
+                sudo ufw allow $PORT
+                echo "UFW configured on port $PORT, SSH passwd authentication disabled, sshd_config changed accordingly. Restart SSH service now."
+                ;;
+        h )
+                echo "ServerHandyMan v 1.0"
+                echo "Simple tool designed to speed up the server preparation process."
+                echo "-u username       adds a new user with elevated privileges"
+                echo "-s        copies the root SSH keys to the new user"
+                echo "-f port_number    changes the SSH port to a random port number, enables UFW, disables the root account and SSH password login"
+                echo "-h        displays this prompt"
+                ;;
+        \? )
+                echo "Invalid prompt. Use [-h] for help."
+                break
+                ;;
+        esac
+done
+shift $((OPTIND -1))
